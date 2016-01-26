ffd1b8dbeef0aa29bab77832b5291f0a843a8d86
jgarciaamor <jgarciaamor@atanasoff01.danielcastelao.org>
2016-01-26 10:10:56 +0100

    183

---
+++ src/boletin18_3/Boletin18_3.java
@@ -0,0 +1,14 @@
+package boletin18_3;
+
+public class Boletin18_3 {
+
+    public static void main(String[] args) {
+        Clase18_3 obxMat = new Clase18_3();
+        obxMat.cargarArray();
+        obxMat.aprobados();
+        obxMat.notaMedia();
+        obxMat.notaAlta();
+        obxMat.visualObj();
+    }
+    
+}
---
+++ src/boletin18_3/Clase18_3.java
@@ -0,0 +1,76 @@
+package boletin18_3;
+
+import javax.swing.JOptionPane;
+
+public class Clase18_3 {
+    int numElementos=pedirElementos();
+    int []notasModulo =new int[numElementos];
+    String []nomes = new String [numElementos];
+    
+    public int pedirElementos(){
+        return Integer.parseInt(JOptionPane.showInputDialog("Introduce el numero de alumnos") );
+    }
+    public String pedirNome(){
+           return JOptionPane.showInputDialog("Introduce el nombre del alumno");
+       }
+    public int ponerNotas(){
+          int notas;
+        do{
+            notas=Integer.parseInt(JOptionPane.showInputDialog("Introduce una nota"));
+            if (notas<1||notas>10)
+                JOptionPane.showMessageDialog(null, "Introduce una nota valida (entre 1 y 10)");
+        } while (notas<1||notas>10);
+        return notas;
+    }
+    public void cargarArray(){
+        for (int i=0;i<notasModulo.length;i++){
+            nomes[i]=pedirNome();
+            notasModulo[i]=ponerNotas();
+            
+        }
+    }
+    public void notaAlta(){
+        int mayor = 0;
+        for (int i=0;i<notasModulo.length;i++){
+            if (notasModulo[i]>mayor){
+                mayor=notasModulo[i];
+            }
+               }
+        System.out.println("La nota mas alta es : "+mayor);
+    }
+    public void notaMedia(){
+        int media=0;
+        
+        for (int i=0;i<notasModulo.length;i++){
+            media=media+notasModulo[i];
+        }       
+        
+        System.out.println("La nota media de la clase es  : "+media/notasModulo.length );
+    }
+    public void aprobados(){
+        int aprobados=0;
+        int suspensos=0;
+        for (int i=0;i<notasModulo.length;i++){
+          if (notasModulo[i]<5){
+              suspensos++;
+              
+          }
+          else {aprobados++;
+          }
+        }
+        System.out.println("numero de suspensos  "+suspensos);
+        System.out.println("numero de aprobados  "+aprobados);
+    }
+    public void visualObj(){
+        int x=0;
+        for (int i=0;i<numElementos;i++){
+            
+        }do{
+            x=Integer.parseInt(JOptionPane.showInputDialog("Introduce la posicion del alumno"));
+            if (x<0||x>numElementos)
+                JOptionPane.showMessageDialog(null, "Introduce una numero entre 1 y "+ numElementos);
+        } while (x<0||x>numElementos);
+                
+        System.out.println("La nota del alumno es : "+notasModulo[x-1] );
+    }
+}
